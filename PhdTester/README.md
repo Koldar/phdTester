b0dfd863490d86de271c4253b4501f3a826de455
Massimo Bono <massimobono1@gmail.com>
2019-02-07 19:54:29 +0100

    online on test pypi

---
+++ PhdTester/README.md
@@ -0,0 +1,45 @@
+
+Introduction
+============
+
+During a Phd, it often arises the need to test *something* (e.g., an algorithm, a framework) over a 
+*combination of parameters*. For instance, in the context of path planning, there are several algorithms, 
+all of them with their wistles and tweaks, you can use to compute the shortest path
+from a source to the target: you can use A* and several heuristics (like landmarks or euclidean distance), or you can use Dijkstra. The performance
+of the algorithm under test may change over the *environment* where it is tested: 
+for example an algorithm may perform better over a map and worse over another one. 
+Even worse, this environment may have its own parameters as well: for instance  environment may have 
+different set of path planning queries (simple, medium, difficult).
+
+Therefore, you often need to test *some stuff* which has its own options over an environment which
+ has its own options as well. Often, you need to generate performances plot over all those combinations.
+
+This problem can be solved by the PhdTester. It's a framework that lets you:
+ 
+1. generate all the test combinations;
+2. plot the result;
+3. and aggregate them in a pdf report.
+ 
+Installation
+============
+
+Installation is very easy, just use:
+
+```
+pip install phd-tester
+```
+
+Requirements
+============
+
+Phd Tester requires some additional tools in order to fully work. For instance, it needs `pdflatex` to generate
+the report. Hence, if you care about the report, be sure to install it. For systems like Ubuntu, this
+can be effectively done via:
+
+```
+sudo apt-get install tex-live
+```
+
+Concepts
+========
+
--- PhdTester/setup.py
+++ PhdTester/setup.py
@@ -1,15 +1,77 @@
-from setuptools import setup
+from typing import Iterable
+
+import semantic_version
+from setuptools import setup, find_packages
+
+VERSION = "0.1.4"
+
+def load_readme() -> str:
+    with open("README.md", "r") as fh:
+        return fh.read()
+
+
+def load_requirements() -> Iterable[str]:
+    useless = [
+        "ansicolors",
+        "bleach",
+        "certifi",
+        "chardet",
+        "coloredlogs",
+        "cycler",
+        "docutils",
+        "humanfriendly",
+        "idna",
+        "kiwisolver",
+        "pkg-resources",
+        "pkginfo",
+        "Pygments",
+        "pyparsing",
+        "python-dateutil",
+        "readme-renderer",
+        "requests",
+        "requests-toolbelt",
+        "setuptools",
+        "six",
+        "tqdm",
+        "twine",
+        "urllib3",
+        "webencodings",
+    ]
+
+    with open("requirements.txt", "r") as fh:
+        for l in fh.readlines():
+            l = l.strip()
+            name = l.split("==")[0]
+            version = semantic_version.Version.coerce(l.split("==")[1])
+            min = version
+            max = version.next_major()
+            if l.split("==")[0] not in useless:
+                result = f"{name}>={min},<{max}"
+                print(f"add {result} to the requirements...")
+                yield result
 
 setup(
-    name='PhdTester',
-    version='2.0.0',
-    packages=[],
-    url='',
-    license='MIT',
+    name='phd-tester',
+    version=VERSION,
     author='Massimo Bono',
     author_email='massimobono1@gmail.com',
     description="""
         Package to generate all possible combinations of your awesome research program. Then it generates plots and a
         pdf report.
-    """
+    """,
+    long_description=load_readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Koldar/phdTester",
+    packages=find_packages(),
+    install_requires=list(load_requirements()),
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Testing",
+        "Intended Audience :: Science/Research",
+    ],
+
+
 )
