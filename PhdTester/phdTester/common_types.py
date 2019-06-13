af5d5364c439c30c14137d07fdf63f94ec1dfe25
Massimo Bono <massimobono1@gmail.com>
2019-06-13 09:55:40 +0200

    issue #88

--- PhdTester/phdTester/common_types.py
+++ PhdTester/phdTester/common_types.py
@@ -1,4 +1,6 @@
 # TODO use these type instad of str!
+import re
+
 KS001Str = str
 PathStr = str
 DataTypeStr = str
@@ -27,3 +29,88 @@ class GetSuchInfo(object):
 
     def __iter__(self):
         return (x for x in [self.path, self.name, self.type, self.ks001, self.tc])
+
+
+class Interval(SlottedClass):
+    """
+    A class representing a mathematical interval between 2 real numbers.
+
+    For example if can represents intervals like the following ones:
+     - (5, 6)
+     - (5, 6]
+     - [5, 6)
+     - [5, 6]
+    """
+    __slots__ = ('__lb', '__ub', '__lb_included', '__ub_included')
+
+    def __init__(self, lb: float, ub: float, lb_included: bool, ub_included: bool):
+        self.__lb = lb
+        self.__lb_included = lb_included
+        self.__ub = ub
+        self.__ub_included = ub_included
+
+    @property
+    def lb(self) -> float:
+        """
+        :return: interval lowebound number
+        """
+
+        return self.__lb
+
+    @property
+    def ub(self) -> float:
+        """
+
+        :return: interval upperbound number
+        """
+        return self.__ub
+
+    @property
+    def lb_included(self) -> bool:
+        """
+        Checks if the lowerbound is included by "[" or by "("
+        :return: true if the lowerbound is included in the interval.
+        """
+        return self.__lb_included
+
+    @property
+    def ub_included(self) -> bool:
+        """
+        Checks if the upperbound is included by "]" or by ")"
+        :return: true if the upperbound is included in the interval.
+        """
+        return self.__ub_included
+
+    def is_in(self, x: float) -> bool:
+        """
+        Checks if a number is included in a given interval
+        :param x: the number involved
+        :return: true if the number is inside an interval, false otherwise
+        """
+        if x == self.lb and self.lb_included:
+            return True
+        if x == self.ub and self.ub_included:
+            return True
+        return self.lb < x < self.ub
+
+    def __str__(self):
+
+        return "{lb_included}{lb}, {ub}{ub_included}".format(
+            lb_included="[" if self.lb_included else "(",
+            lb=self.lb,
+            ub=self.ub,
+            ub_included="]" if self.lb_included else ")",
+        )
+
+    @classmethod
+    def parse(cls, string: str) -> "Interval":
+        m = re.match(r"^\s*(?P<lb_included>[\(\[\)\]])\s*(?P<lb>[\+\-]?\d+\.?\d*)\s*,\s*(?P<ub>[\+\-]?\d+\.?\d*)\s*(?P<ub_included>[\(\[\)\]])\s*$", string)
+        if m is None:
+            raise ValueError(f"cannot parse \"{string}\" into {Interval.__class__.__name__}!")
+        lb_included = m.group("lb_included") in ["[", ")"]
+        ub_included = m.group("ub_included") in ["]", "("]
+        lb = float(m.group("lb"))
+        ub = float(m.group("ub"))
+
+        return Interval(lb=lb, ub=ub, lb_included=lb_included, ub_included=ub_included)
+
