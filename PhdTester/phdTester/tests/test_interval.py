8c1ab54bbe6b0f139f9a2f71cf1cfd86846269a3
Massimo Bono <massimobono1@gmail.com>
2019-06-13 10:06:14 +0200

    issuec #88

--- PhdTester/phdTester/tests/test_interval.py
+++ PhdTester/phdTester/tests/test_interval.py
@@ -1,9 +1,50 @@
 import unittest
 
+from phdTester import common_types
+from phdTester.common_types import Interval
+
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)
+        self.assertEqual(Interval.parse("[5,6]"), Interval(5.0, 6.0, True, True))
+        self.assertEqual(Interval.parse("[5,6)"), Interval(5, 6, True, False))
+        self.assertEqual(Interval.parse("(5,6]"), Interval(5, 6, False, True))
+        self.assertEqual(Interval.parse("(5,6)"), Interval(5, 6, False, False))
+        self.assertEqual(Interval.parse("]5,6]"), Interval(5, 6, False, True))
+        self.assertEqual(Interval.parse("[5,6["), Interval(5, 6, True, False))
+        self.assertEqual(Interval.parse("]5,6["), Interval(5, 6, False, False))
+
+    def test_contains_01(self):
+        a = Interval(5, 10, True, True)
+        self.assertEqual(a.contains(6), True)
+        self.assertEqual(a.contains(5), True)
+        self.assertEqual(a.contains(10), True)
+        self.assertEqual(a.contains(1), False)
+        self.assertEqual(a.contains(20), False)
+
+    def test_contains_02(self):
+        a = Interval(5, 10, False, True)
+        self.assertEqual(a.contains(6), True)
+        self.assertEqual(a.contains(5), False)
+        self.assertEqual(a.contains(10), True)
+        self.assertEqual(a.contains(1), False)
+        self.assertEqual(a.contains(20), False)
+
+    def test_contains_03(self):
+        a = Interval(5, 10, True, False)
+        self.assertEqual(a.contains(6), True)
+        self.assertEqual(a.contains(5), True)
+        self.assertEqual(a.contains(10), False)
+        self.assertEqual(a.contains(1), False)
+        self.assertEqual(a.contains(20), False)
+
+    def test_contains_04(self):
+        a = Interval(5, 10, False, False)
+        self.assertEqual(a.contains(6), True)
+        self.assertEqual(a.contains(5), False)
+        self.assertEqual(a.contains(10), False)
+        self.assertEqual(a.contains(1), False)
+        self.assertEqual(a.contains(20), False)
 
 
 if __name__ == '__main__':
