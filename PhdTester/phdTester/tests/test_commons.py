6159b555efb43fdd7a3a24b409dd0ccdc3fe4ce8
Massimo Bono <massimobono1@gmail.com>
2019-07-09 09:37:19 +0200

    issue #115

--- PhdTester/phdTester/tests/test_commons.py
+++ PhdTester/phdTester/tests/test_commons.py
@@ -1,12 +1,40 @@
+import abc
 import unittest
 
 from phdTester import commons
 
 
+class IFoo(abc.ABC):
+
+    @abc.abstractmethod
+    def foo(self, name: str) -> str:
+        pass
+
+
+class FooImpl(IFoo):
+
+    def foo(self, name: str) -> str:
+        return f"{name} FooImpl"
+
+
+def function_foo(name: str) -> str:
+    return f"{name} function_foo"
+
+
 class MyTestCase(unittest.TestCase):
+
     def test_expand_string(self):
         path = commons.expand_string("csvs/arena.map", [('/', '-')])
         self.assertEqual(path, "csvs-arena.map")
+
+    def test_direct_call_or_method_call(self):
+
+        foo = FooImpl()
+
+        self.assertEqual(commons.direct_call_or_method_call(foo, "foo", "world"), f"world FooImpl")
+        self.assertEqual(commons.direct_call_or_method_call(foo, IFoo.foo, "world"), f"world FooImpl")
+        self.assertEqual(commons.direct_call_or_method_call(function_foo, "foo", "world"), f"world function_foo")
+        self.assertEqual(commons.direct_call_or_method_call(lambda name: f"{name} lambda", "foo", "world"), f"world lambda")
 
 
 if __name__ == '__main__':
